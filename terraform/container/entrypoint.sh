33ecf86d92f7c41a67d5698f80e10c0675aea4f4
moritz <moritz.hueper@posteo.net>
2026-02-11 06:26:28 +0100

    feat(tgi): set max input and total token limits

--- terraform/container/entrypoint.sh
+++ terraform/container/entrypoint.sh
@@ -13,4 +13,6 @@ fi
 exec /usr/local/bin/text-generation-launcher \
   --model-id "${HF_MODEL_ID}" \
   --num-shard "${SM_NUM_GPUS:-1}" \
-  --port 8080
+  --port 8080 \
+  --max-input-length 4096 \
+  --max-total-tokens 5120
